e55cdeb6656cd409f4249bd4fe5ddf8a2b6a3081
Nagarajar <nagarajar70@gmail.com>
2022-04-16 13:03:41 +0530

    Initial commit on democomponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],styles:['color:red;']
 })
 export class AppComponent {
   title = 'democomponents';
+  isDisabled = false;
+  active = true;
+  n1= 0;
+  n2 = 0;
+
+  add(n1:number,n2?:number){
+    if(n2)
+    {
+      return n1 + n2;
+    }
+    return n1;
+  }
+  like(){
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImgcomponentComponent } from './imgcomponent/imgcomponent.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextcomponentComponent,
+    ImgcomponentComponent,
+    FormComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
